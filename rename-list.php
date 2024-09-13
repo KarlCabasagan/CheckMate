130c0826142f699ae233aa9319783ee927fd2567
Karl Cabasagan <karl.cabasagan.202200008@gmail.com>
2024-09-13 09:10:58 +0800

    Added error message in the rename list page

--- rename-list.php
+++ rename-list.php
@@ -46,11 +46,17 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             </div>
         </div>
         <div class="list-name-wrapper">
-            <div class="name-input-wrapper">
-                <form id="rename-form" action="./data-handling/rename-list.php" method="POST">
-                    <input type="hidden" name="list-id" id="list-id" value="<?php echo $listId?>">
-                    <input type="text" name="list-name" id="list-name" value="<?php echo $list_name ?>" required>
-                </form>
+            <div class="list-name-inner-wrapper">
+                <div class="name-input-wrapper">
+                    <form id="rename-form" action="./data-handling/rename-list.php" method="POST">
+                        <input type="hidden" name="list-id" id="list-id" value="<?php echo $listId?>">
+                        <input type="text" name="list-name" id="list-name" value="<?php echo $list_name ?>" required>
+                    </form>
+                </div>
+                <div class="error-message-wrapper">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#ff3030"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                    <span id="error-message">Please enter a name</span>
+                </div>
             </div>
         </div>
     </div>
@@ -65,7 +71,9 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
                     document.getElementById('rename-form').submit();                    
                 } else {
                     const formWrapper = document.querySelector('.name-input-wrapper');
+                    const errorMessage = document.querySelector('.error-message-wrapper');
                     formWrapper.style.border = "solid #ff3030 3px"
+                    errorMessage.style.visibility = "visible";
                 }
             });
         });
